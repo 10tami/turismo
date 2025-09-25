33f650e5f6e762e87bc056d9e6df06791de1e508
10tami <melinao10tami@gmail.com>
2025-09-25 19:56:31 -0300

    separe los componentes de las paginas

---
+++ src/paginas/Holis.jsx
@@ -0,0 +1,31 @@
+
+
+const Holis = () => {
+  return (
+    <div className="bg-cyan-300">
+    Turismo ESquel 2025
+
+
+    </div>
+
+
+
+
+  )
+}
+
+export default Holis
+
+
+
+
+
+
+
+
+
+
+
+
+
+
